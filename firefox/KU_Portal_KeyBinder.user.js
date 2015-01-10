d8022e4442b0a26ccb107d250ba57c8292aae11a
k-ashiya <kazuki.ashiya@gmail.com>
2015-01-11 00:24:35 +0900

    Greasemonkey用スクリプトを作成

---
+++ firefox/KU_Portal_KeyBinder.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        KU Portal KeyBinder
+// @namespace   http://hanakimi-ikepara.hotcom-web.com
+// @description 京都大学の全学生共通ポータルにログイン後の画面で、キー操作一つで各リンクへ飛ぶためのExtension
+// @include     https://student.iimc.kyoto-u.ac.jp/list.html
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+$(document).keydown(function(e){
+	c = e.keyCode;
+	switch (c) {
+		case 69:
+			open($("#elearning p:first a").attr("href"), "_blank");
+			break;
+		case 75:
+			open($("#kulasis p:first a").attr("href"), "_blank");
+			break;
+		case 76:
+			open($("#mykuline p:first a").attr("href"), "_blank");
+			break;
+		case 77:
+			open($("#kumoi p:first a").attr("href"), "_blank");
+			break;
+		case 80:
+			open($("#panda p:first a").attr("href"), "_blank");
+			break;
+		default:
+			break;
+	}
+});
