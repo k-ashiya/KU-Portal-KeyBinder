ed55401a20a6f673cb17d71b328d9a0194b41671
k-ashiya <kazuki.ashiya@gmail.com>
2015-01-23 17:01:56 +0900

    仕様変更に対応

--- firefox/KU_Portal_KeyBinder.user.js
+++ firefox/KU_Portal_KeyBinder.user.js
@@ -3,7 +3,7 @@
 // @namespace   http://hanakimi-ikepara.hotcom-web.com
 // @description 京都大学の全学生共通ポータルにログイン後の画面で、キー操作一つで各リンクへ飛ぶためのExtension
 // @include     https://student.iimc.kyoto-u.ac.jp/list.html
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
 
@@ -11,19 +11,19 @@ $(document).keydown(function(e){
 	c = e.keyCode;
 	switch (c) {
 		case 69:
-			open($("#elearning p:first a").attr("href"), "_blank");
+			open($("#elearning").prev().children("a").attr("href"), "_blank");
 			break;
 		case 75:
-			open($("#kulasis p:first a").attr("href"), "_blank");
+			open($("#kulasis").prev().children("a").attr("href"), "_blank");
 			break;
 		case 76:
-			open($("#mykuline p:first a").attr("href"), "_blank");
+			open($("#mykuline").prev().children("a").attr("href"), "_blank");
 			break;
 		case 77:
-			open($("#kumoi p:first a").attr("href"), "_blank");
+			open($("#kumoi").prev().children("a").attr("href"), "_blank");
 			break;
 		case 80:
-			open($("#panda p:first a").attr("href"), "_blank");
+			open($("#panda").prev().children("a").attr("href"), "_blank");
 			break;
 		default:
 			break;
--- js/content.js
+++ js/content.js
@@ -2,19 +2,19 @@ $(document).keydown(function(e){
 	c = e.keyCode;
 	switch (c) {
 		case 69:
-			open($("#elearning p:first a").attr("href"), "_blank");
+			open($("#elearning").prev().children("a").attr("href"), "_blank");
 			break;
 		case 75:
-			open($("#kulasis p:first a").attr("href"), "_blank");
+			open($("#kulasis").prev().children("a").attr("href"), "_blank");
 			break;
 		case 76:
-			open($("#mykuline p:first a").attr("href"), "_blank");
+			open($("#mykuline").prev().children("a").attr("href"), "_blank");
 			break;
 		case 77:
-			open($("#kumoi p:first a").attr("href"), "_blank");
+			open($("#kumoi").prev().children("a").attr("href"), "_blank");
 			break;
 		case 80:
-			open($("#panda p:first a").attr("href"), "_blank");
+			open($("#panda").prev().children("a").attr("href"), "_blank");
 			break;
 		default:
 			break;
